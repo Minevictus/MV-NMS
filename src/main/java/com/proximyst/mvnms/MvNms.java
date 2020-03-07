be622f36ef00d372e47b2b4a647e2ed250d1d3d2
Mariell Hoversholm <proximyst@proximyst.com>
2020-03-07 22:53:29 +0100

    Disable plugin upon unsupported version

--- src/main/java/com/proximyst/mvnms/MvNms.java
+++ src/main/java/com/proximyst/mvnms/MvNms.java
@@ -5,6 +5,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class MvNms extends JavaPlugin {
   @Override
   public void onEnable() {
+    if (BukkitVersion.isUnknownVersion()) {
+      getLogger().severe("Found version " + BukkitVersion.rawBukkitVersion + ", which is not supported.");
+      getLogger().severe("The plugin will not function and any dependents might not work correctly.");
+      getLogger().warning("Disabling plugin...");
+      setEnabled(false);
+      return;
+    }
+
     // Set up the current version if it exists.
     BukkitVersion.getOptionalVersion()
         .ifPresent(BukkitVersion::setup);
