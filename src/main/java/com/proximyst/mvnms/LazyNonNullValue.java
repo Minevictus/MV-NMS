54a189f5333b14e27e4a7969bb1418f451605c11
Mariell Hoversholm <proximyst@proximyst.com>
2020-04-08 19:14:33 +0200

    Use lazily created values for versions

---
+++ src/main/java/com/proximyst/mvnms/LazyNonNullValue.java
@@ -0,0 +1,42 @@
+package com.proximyst.mvnms;
+
+import java.util.Objects;
+import java.util.function.Supplier;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+class LazyNonNullValue<T> {
+  private final Object synchronizedLock = new Object();
+
+  @NotNull private final Supplier<T> createValue;
+  @Nullable private T value = null;
+
+  LazyNonNullValue(@NotNull Supplier<T> createValue) {
+    Objects.requireNonNull(createValue, "createValue cannot be null");
+
+    this.createValue = createValue;
+  }
+
+  void eager() {
+    if (value == null) {
+      synchronized (synchronizedLock) {
+        if (value == null) {
+          value = createValue.get();
+        }
+      }
+    }
+  }
+
+  @NotNull
+  T getValue() {
+    if (value == null) {
+      synchronized (synchronizedLock) {
+        if (value == null) {
+          return value = createValue.get();
+        }
+      }
+    }
+
+    return value;
+  }
+}
